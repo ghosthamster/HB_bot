c3d8af3cb58f7471871c148a9594dbcbaecd282b
Max <maxpopovych123@gmail.com>
2020-01-14 20:31:15 +0200

    bug fix

--- bot_start.py
+++ bot_start.py
@@ -1,9 +1,9 @@
-from telegram.ext import Updater, CommandHandler,MessageHandler,Filters, ConversationHandler, RegexHandler
+from telegram.ext import Updater, CommandHandler,MessageHandler,Filters, ConversationHandler
 import logging
 import sqlite3
 
 #bot_initialize
-updater = Updater(token='YOUR TOKEN HEAR', use_context=True)
+updater = Updater(token='910009672:AAEEvUKd4JcAmGMxjqxi0wtd0I3O_mzS9VM', use_context=True)
 dispatcher = updater.dispatcher
 POLL = 1
 
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 #bot_functionality
 def bot_start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="hi, just write use /add_birthday to add your birthday")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Драсте! Я ДН-БОТ_V1. Я запам'ятовую дати ваших народжень (якщо поможете) і нагадую Вам ,що ви на 1 рік ближче до 💀 .")
 
 def bot_unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Ти шо? Курча, мене зламати хочеш? Хуй тобі! 🖕")
@@ -24,27 +24,36 @@ def bot_new_member(update,context):
                 db = sqlite3.connect("birthday.db")
                 curs = db.cursor()
                 ident = "table" + str(abs(update.effective_chat.id))
-                curs.execute("""CREATE TABLE IF NOT EXISTS """ + str(ident) + """ (id INT PRIMARY KEY, birthdate DATE)""")
+                curs.execute("""CREATE TABLE IF NOT EXISTS """ + str(ident) + """ (id INT PRIMARY KEY, day INT, month INT, year INT)""")
             create_database_table()
-            context.bot.send_message(chat_id=update.effective_chat.id, text="hi, just write /start to start work with me")
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Драсте! Я ДН-БОТ_V1. Я запам'ятовую дати ваших народжень (якщо поможете) і нагадую вам ,що ви на 1 рік ближче до 💀 . Для початку роботи зі мною пропишіть /start")
             break
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="hi, use /add_birthday")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Я бачу у вас тут новий ПІДОР. Ну здарасте, здрасте. Я ДН-БОТ. Не забудь добавити мені свою дату народження. ОК? Бо заріжу 🤪")
 
 def bot_add_birthday(update,context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=""enter u birthday date?\n'Birthday:YYYY-MM-DD'")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Коли ж ти народився, а?\nВведи дату в такому форматі DD.MM.YYYY")
+    return POLL
+
+def bot_chat_poll(update,context):
     msg = update.effective_message.text
+    msg = msg.split('.')
     print(msg)
-        def insert_database_table():
-            db = sqlite3.connect("birthday.db")
-            curs = db.cursor()
-            user_id = str(abs(update.effective_user.id))
-            ident = "table" + str(abs(update.effective_chat.id))
-            print("""INSERT INTO """ + str(ident) + """ (id, birthdate) VALUES ('""" + user_id + """','""" + msg + """');""")
-            curs.execute("""INSERT INTO """ + str(ident) + """ (id, birthdate) VALUES ('""" + user_id + """','""" + msg + """');""")
-        insert_database_table()
+    def insert_database_table():
+        db = sqlite3.connect("birthday.db")
+        curs = db.cursor()
+        user_id = str(abs(update.effective_user.id))
+        ident = "table" + str(abs(update.effective_chat.id))
+        curs.execute("""INSERT INTO """ + str(ident) + """ (id, day, month, year) VALUES ("""  + user_id +  ""","""  + msg[0] + """,""" + msg[1] + """,""" + msg[2] + """)""")
+        db.commit()
+    insert_database_table()
+    return ConversationHandler.END
+
+def bot_add_birthday_cancel(update,context):
+    return ConversationHandler.END
 
 def bot_chat_kick(update,context):
+    print("DELETED")
     if update.message.left_chat_member.username == 'HB_chek_bot':
         def delete_database_table():
             db = sqlite3.connect("birthday.db")
@@ -54,11 +63,10 @@ def bot_chat_kick(update,context):
         delete_database_table()
 
 #bot_hadlers_creation
-
 new_member_handler = MessageHandler(Filters.status_update.new_chat_members,bot_new_member)
 bot_chat_kick_handler = MessageHandler(Filters.status_update.left_chat_member,bot_chat_kick)
 start_handler = CommandHandler('start', bot_start)
-add_birthday_handler = CommandHandler('add_birthday', bot_add_birthday)
+add_birthday_handler = ConversationHandler(entry_points = [CommandHandler('add_birthday',bot_add_birthday)], states = {POLL: [MessageHandler(Filters.text,bot_chat_poll)]}, fallbacks = [CommandHandler('cancel',bot_add_birthday_cancel)])
 unknown_handler = MessageHandler(Filters.command, bot_unknown)
 
 #bot_hadlers_registration
