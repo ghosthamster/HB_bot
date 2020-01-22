a6b0f21e1904caf79f51ae59ba6e578c9ea8a7db
Max <maxpopovych123@gmail.com>
2020-01-22 19:26:29 +0200

    Update bot_start.py

--- bot_start.py
+++ bot_start.py
@@ -1,11 +1,12 @@
 from bot_func import *
 from telegram.ext import MessageHandler, PrefixHandler, Dispatcher,JobQueue, Filters, CommandHandler
+import re
 
 #logging
 #logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 def main():
-    updater = Updater(token='YOUR BOT TOKEN', use_context=True)
+    updater = Updater(token='910009672:AAEEvUKd4JcAmGMxjqxi0wtd0I3O_mzS9VM', use_context=True)
     dispatcher = updater.dispatcher
 
     #bot_hadlers_creation
@@ -14,7 +15,7 @@ def main():
     bot_del_handler = ConversationHandler([PrefixHandler('🧹','DELETE🧹',bot_del)],{request_del:[MessageHandler(Filters.text,bot_request_del)]},[])
     bot_change_handler = ConversationHandler([PrefixHandler('✏️','CHANGE✏️',bot_change)],{request_change:[MessageHandler(Filters.text,bot_request_change)]},[])
     bot_show_handler = ConversationHandler([PrefixHandler('🔎','SHOW🔎',bot_show)], {request_show:[PrefixHandler('🌎','ALL🌎',bot_show_all),PrefixHandler('◀️','BACK◀️',bot_cancel),PrefixHandler('📆','CURRENT📆',bot_show_current_month),ConversationHandler([PrefixHandler('🥳','FRIENDS🥳',bot_show_friends)],{friends_show:[MessageHandler(Filters.text,bot_request_show_friends)]},[],map_to_parent= {ConversationHandler.END : ConversationHandler.END })]},[])
-    bot_settings_handler = ConversationHandler([PrefixHandler('⚙️','SETTINGS⚙️',bot_settings)],{request_settings:[PrefixHandler('◀️','BACK◀️',bot_cancel),ConversationHandler([]),ConversationHandler([PrefixHandler('📧','FEEDBACK📧',bot_feedback)],{feedback_request : [MessageHandler(Filters.text,bot_feedback_request)]},[],map_to_parent= {ConversationHandler.END : ConversationHandler.END })]},[])
+    bot_settings_handler = ConversationHandler([PrefixHandler('⚙️','SETTINGS⚙️',bot_settings)],{request_settings:[PrefixHandler('◀️','BACK◀️',bot_cancel),ConversationHandler([PrefixHandler('➕','ADD_CUSTOM_WISHES➕',bot_add_wish)],{request_add_wish : [MessageHandler(Filters.text,bot_add_wish_request)]},[],map_to_parent={ConversationHandler.END : ConversationHandler.END}),ConversationHandler([PrefixHandler('☄️','USE_CUSTOM_WISHES☄️',bot_custom_wishes)], {custom_wishes_request : [MessageHandler(Filters.text,bot_custom_wishes_request)]},[],map_to_parent= {ConversationHandler.END : ConversationHandler.END }),ConversationHandler([PrefixHandler('📧','FEEDBACK📧',bot_feedback)],{feedback_request : [MessageHandler(Filters.text,bot_feedback_request)]},[],map_to_parent= {ConversationHandler.END : ConversationHandler.END })]},[])
     bot_left_chat_handler = MessageHandler(Filters.status_update.left_chat_member,bot_left_chat)
 
     #bot_hadlers_registration
@@ -29,12 +30,14 @@ def main():
     #Job
     file = open("tasker.txt","r+")
     check = file.read()
-
     if not check == str(datetime.datetime.now().date()):
         Tasker = updater.job_queue
         Tasker.set_dispatcher(updater.dispatcher)
         Tasker.run_once(bot_reminder,2)
         Tasker.run_daily(bot_reminder,datetime.time(0,0,1,0))
+        file.close()
+        
+        file = open("tasker.txt","w")
         file.write(str(datetime.datetime.now().date()))
 
     file.close()
